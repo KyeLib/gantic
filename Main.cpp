ac622e081435966848aeabcfe38fb0ffbba14eb0
kye <libbykye@gmail.com>
2026-01-12 16:51:42 -0700

    Created function to get local time

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,31 @@
+#include <chrono>
 #include <iostream>
+
+
+
+//Gets current time
+//Will move in future
+std::string getCurTime()
+{
+
+    auto getTime{ std::chrono::system_clock::now() };
+
+    std::time_t timeNow{ std::chrono::system_clock::to_time_t(getTime) };
+
+    std::tm localTime{};
+    localtime_s(&localTime, &timeNow);
+
+    std::ostringstream oss;
+    oss << std::put_time(&localTime, "%c");
+
+    return oss.str();
+
+}
+
 
 int main()
 {
 
-	std::cout << "Helloo World";
-
-	return 0;
-
+    std::cout << getCurTime();
+       
 }
